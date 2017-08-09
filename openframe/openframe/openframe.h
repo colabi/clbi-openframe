d930a463b10dc776246613758405510efd1df8ef
piezas <seth@colabi.com>
2017-08-09 12:16:32 -0700

    initial

---
+++ openframe/openframe/openframe.h
@@ -0,0 +1,19 @@
+//
+//  openframe.h
+//  openframe
+//
+//  Created by seth piezas on 8/9/17.
+//  Copyright © 2017 seth piezas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for openframe.
+FOUNDATION_EXPORT double openframeVersionNumber;
+
+//! Project version string for openframe.
+FOUNDATION_EXPORT const unsigned char openframeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <openframe/PublicHeader.h>
+
+
